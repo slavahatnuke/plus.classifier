3e01483225dd16aa76ab507360ac0143b3502d5e
slava hatnuke <slava.hatnuke@gmail.com>
2017-11-11 04:40:59 +0200

    ^

--- x.js
+++
@@ -1,7 +0,0 @@
-const {Classifier} = require('./index');
-const classifier = Classifier([
-  ['<h1> hey </h1>', 'title'],
-  ['<h2> hey </h2>', 'description']
-]);
-
-console.log(classifier.classification('h2 hey'))
